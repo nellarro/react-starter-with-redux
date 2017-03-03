b51aa0eab6ac8f74e5fed0b8ce8fa1701f29b959
Evan Klemen <evan.klemen@wbdcorp.com>
2017-03-03 15:58:31 -0500

    removed old dependecy css imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import 'react-select/dist/react-select.css';
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import './index.scss';
 
 import { Provider } from "react-redux";
